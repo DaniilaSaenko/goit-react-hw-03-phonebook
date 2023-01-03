bcdb4f9c2cc8d807e1d28aea3092a3fa15780c39
DaniilaSaenko <daniila.s777@gmail.com>
2023-01-03 23:35:49 +0200

    add localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,27 @@ import { Box } from './Box';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+componentDidMount() {
+    const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
+
+    if (parsedContacts) {
+      this.setState({
+        contacts: parsedContacts,
+      });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { contacts } = this.state;
+
+    if (prevState.contacts !== contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
 
   addContacts = data => {
     const { name, number } = data;
@@ -67,7 +80,10 @@ export class App extends Component {
     return (
       <Box px={20} pt={10}>
         <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.addContacts} />
+        <ContactForm
+          onSubmit={this.addContacts}
+          contacts={this.state.contacts}
+        />
 
         {contacts.length !== 0 && (
           <Box mt={20}>
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -8,10 +8,17 @@ export class ContactForm extends Component {
     number: '',
   };
 
-  static propTypes = {
+static propTypes = {
     onSubmit: PropTypes.func.isRequired,
+    contacts: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        number: PropTypes.string.isRequired,
+      }).isRequired
+    ).isRequired,
   };
-
+  
   handleInputChange = evt => {
     const { name, value } = evt.target;
 
@@ -28,6 +35,21 @@ export class ContactForm extends Component {
       name: '',
       number: '',
     });
+
+    const { contacts } = this.props;
+    const {
+      elements: { number, name },
+    } = evt.target;
+
+    contacts.map(contact => {
+      if (contact.name === name.value) {
+        this.setState({
+          number: number.value,
+        });
+      }
+
+      return this.state;
+    });
   };
 
   render() {
@@ -42,7 +64,6 @@ export class ContactForm extends Component {
               type="text"
               name="name"
               value={name}
-              placeholder="Name"
               onChange={this.handleInputChange}
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
@@ -56,7 +77,6 @@ export class ContactForm extends Component {
               type="tel"
               name="number"
               value={number}
-              placeholder="Phone number"
               onChange={this.handleInputChange}
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
